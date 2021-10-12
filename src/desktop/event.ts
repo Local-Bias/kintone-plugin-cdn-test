383726aab4d833f95890fcf828d96b829fd74647
Ribbit <echo07200503@gmail.com>
2021-10-12 17:49:46 +0900

    CDN実験

--- src/desktop/event.ts
+++ src/desktop/event.ts
@@ -5,7 +5,7 @@ const events: kintone.EventType[] = ['mobile.app.record.edit.show'];
 const action: launcher.Action = async (event, pluginId) => {
   const config = restoreStorage(pluginId);
 
-  console.log('プラグインが有効です', { pluginId, event, config });
+  console.log('プラグインのリソースを、GitHub経由で実行しました！', { pluginId, event, config });
   return event;
 };
 
